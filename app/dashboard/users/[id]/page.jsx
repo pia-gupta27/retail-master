478708483e6d97eca139fd3d139d366d8acddf00
pia-gupta27 <pia27g@gmail.com>
2024-07-24 08:47:27 +0530

    Update page.jsx

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,6 +1,6 @@
 import { updateUser } from '@/app/lib/actions';
 import { fetchUser } from '@/app/lib/data';
-import styles from '@/app/ui/dashboard/users/singleUser/singleuser.module.css'
+import styles from '@/app/ui/dashboard/users/singleUser/singleUser.module.css'
 import Image from "next/image";
 
 const SingleUserPage = async ({params}) => {
